0622fcd49660fd0203cf6df1525e6b1e3381f2e9
Mateus Zamorano <52588123+lorde6999@users.noreply.github.com>
2024-05-30 15:51:14 -0300

    Add files via upload

---
+++ Segundo Desafio Felipão/SegundoDesafioFelipão.js
@@ -0,0 +1,7 @@
+let vitoria = 10
+let derrota = 10
+let heroi
+heroi = ("SH4NK")
+const experiencia = ["Ferro", " Bronze - PDL - 11", " Prata - PDL - 21", " Ouro - PDL - 51", " Diamante - PDL - 81", " Lendário - PDL - 91", " Imortal - PDL - 101"];
+const message = ": O Herói tem de saldo de vitórias " + vitoria + " e está no nível "
+console.log (heroi + message + experiencia[0])
